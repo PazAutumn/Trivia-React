2d44f59c2622f4815aef4df6e6184c5cc37dd6f2
IsmaryAlvarez <ismaryad@gmail.com>
2018-03-14 12:24:19 -0300

    Creando Componente TriviaContainer

--- src/App.js
+++ src/App.js
@@ -1,20 +1,35 @@
-import React, { Component } from 'react';
-import Logo from './components/Logo'
+import React, { Component } from "react";
+import Logo from "./components/Logo";
+import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
+import { Grid, Row, Col } from "react-flexbox-grid";
 
-import './App.css';
+import "./App.css";
+import TriviaContainer from "./components/TriviaContainer";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <Logo></Logo>
-          <h1 className="App-title">titulo</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <MuiThemeProvider>
+        <Grid>
+          <Row>
+            <Col xs={12}>
+              <div className="App">
+                <header className="App-header">
+                  <Logo />
+                  <h1 className="App-title">Let's Start The Game</h1>
+                </header>
+              </div>
+            </Col>
+          </Row>
+          <Row>
+          <Col xs={12}>
+              <div>
+                  <TriviaContainer />
+              </div>
+            </Col>
+          </Row>
+        </Grid>
+      </MuiThemeProvider>
     );
   }
 }
