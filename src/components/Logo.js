d5cfbd4ffbd2d5a3781602018c37776afb44304f
IsmaryAlvarez <ismaryad@gmail.com>
2018-03-14 10:45:29 -0300

    Creando componente Logo

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -1,30 +1,9 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import WeatherLocation from './WeatherLocation';
-import './styles.css';
+import React from "react";
 
-const LocationList = ( {cities, onSelectedLocation} ) => {
+const Logo = Logo => (
+  <div className="logo">
+    <img src={require("./img/logo.png")} alt="Logo Trivia" />
+  </div>
+);
 
-  const handleWeatherLocationClick = (city) => {
-    console.log(handleWeatherLocationClick);
-    onSelectedLocation(city);
-  }
-
-  const strToComponent = (cities) => (
-    cities.map(city => (
-      <WeatherLocation key = {city} city = {city} onWeatherLocationClick = {() => handleWeatherLocationClick(city)} />
-    ))
-  )
-  return (
-    <div className="locationList">
-      {strToComponent(cities)}
-    </div>
-  )
-}
-
-LocationList.propTypes = {
-  cities: PropTypes.array.isRequired,
-  onSelectedLocation: PropTypes.func,
-}
-
-export default LocationList;
+export default Logo;
