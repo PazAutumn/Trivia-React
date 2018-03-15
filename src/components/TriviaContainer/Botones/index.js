6ec2fa9cf1aa3b3ce370c60882e81eb5ed430fc6
IsmaryAlvarez <ismaryad@gmail.com>
2018-03-15 12:24:32 -0300

    Actualizando llamado API

--- src/components/TriviaContainer/Botones/index.js
+++ src/components/TriviaContainer/Botones/index.js
@@ -1,22 +1,49 @@
-import React from "react";
+import React, { Component } from "react";
 import PropTypes from "prop-types";
 import { Grid, Row, Col } from "react-flexbox-grid";
 import "./style.css";
 
-const Botones = () => (
-  <div className="botones">
-    <Grid>
-      <Row>
-        <Col xs={6}>
-          <input type="button" value="True"/>
-        </Col>
-        <Col xs={6}>
-          <input type="button" value="False"/>
-        </Col>
-      </Row>
-    </Grid>
-  </div>
-);
+class Botones extends Component {
+
+  constructor(){
+    super();
+    this.state = {
+      respuesta: null,
+    }
+  }
+
+  componentWillMount(){
+    fetch('https://opentdb.com/api.php?amount=10&category=11&type=boolean')
+      .then(response => {
+        console.log(response);
+        return response.json();
+      }).then(data => {
+        let respuesta = data.results[0].correct_answer;
+        console.log(respuesta);
+        this.setState({respuesta});
+      })
+    }
+
+
+  render() {
+    return(
+      <div class="botones">
+        <Grid>
+          <Row>
+            <Col xs={6}>
+              <input type="button" value="True"/>
+            </Col>
+            <Col xs={6}>
+              <input type="button" value="False"/>
+            </Col>
+          </Row>
+        </Grid>
+      </div>
+    )
+  }
+}
+
+
 
 Botones.propTypes = {};
 
--- src/components/TriviaContainer/Preguntas/index.js
+++ src/components/TriviaContainer/Preguntas/index.js
@@ -1,24 +1,45 @@
-import React from "react";
+import React, { Component } from "react";
 import PropTypes from "prop-types";
 import { Grid, Row, Col } from "react-flexbox-grid";
 import "./style.css";
 
-const Preguntas = () => (
-  <div className="questions">
-    <Grid>
-      <Row>
-        <Col xs={12}>
-          <p># Preguntas Aqui</p>
-        </Col>
-      </Row>
-      <Row>
-        <Col xs={12}>
-          <p>Preguntas Aqui</p>
-        </Col>
-      </Row>
-    </Grid>
-  </div>
-);
+class Preguntas extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      questions: null,
+    }
+  }
+
+  componentWillMount() {
+      fetch('https://opentdb.com/api.php?amount=10&category=11&type=boolean')
+      .then(response => {
+        console.log(response);
+        return response.json();
+      }).then(data => {
+        let questions = data.results[0].question;
+        console.log(questions);
+        this.setState({questions});
+      })
+    }
+
+  render() {
+    console.log('Render');
+    const { questions } = this.state;
+    return(
+      <div className="questions-cont">
+        <Grid>
+          <Row>
+            <Col xs={12}>
+              <p>{questions}</p>
+            </Col>
+          </Row>
+        </Grid>
+      </div>
+    )
+  }
+}
 
 Preguntas.propTypes = {};
 
